0f40a19b7be3ff36caf96277d20fb7ab39e19ded
ruzelyanet <ruzelyanet@mail.ru>
2016-11-03 17:53:18 +0500

    change version

--- iceseo/markup/static/js/separate-js/app.js
+++ iceseo/markup/static/js/separate-js/app.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
+		
+	$(".big-baner-bg").fallings({
+		velocity: .4,
+        initialPosition: -50,
+        bgParallax: true,
+		bgPercent: '50%'
+	});
+
+	$(".media-cup").fallings({
+		velocity: .3,
+        initialPosition: 100,
+        bgParallax: false,
+		bgPercent: '50%'
+	});
+	
+	$(".media-pen").fallings({
+		velocity: .4,
+        initialPosition: 300,
+        bgParallax: false,
+		bgPercent: '50%'
+	});
 
 	$(".keys-list").owlCarousel({		
 		items : 2		
